e5c4f8b656d93ac112ceceeb8fff355476ccfedb
Daniel Cherino <daniel.cherino@telegraph.co.uk>
2017-09-12 14:51:10 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+}
